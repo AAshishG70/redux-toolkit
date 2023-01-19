8e8471a4d96244c929c9594fc190737f11221600
Your Name <you@example.com>
2023-01-19 15:06:44 +0545

    solve reaction feature

--- src/components/display-components/post-author.jsx
+++ src/components/display-components/post-author.jsx
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux";
+import { selectAllUsers } from "../../features/users/user-slice";
+
 const PostAuthor = ({ userId }) => {
-    const users = [
-      { id: "0", name: "Dude Lebowski" },
-      { id: "1", name: "Neil Young" },
-      { id: "2", name: "Dave Gray" },
-    ];
-  
-    const author = users.find((user) => user.id === userId);
-  
-    return <span>by {author ? author.name : "Unknown author"}</span>;
-  };
-  export default PostAuthor;
+  const users = useSelector(selectAllUsers);
+  console.log(users);
+
+  console.log(userId);
+
+  const author = users.find((user) => +user.id === userId);
+
+  console.log(author);
+
+  return <span>by {author ? author.name : "Unknown author"}</span>;
+};
+export default PostAuthor;
